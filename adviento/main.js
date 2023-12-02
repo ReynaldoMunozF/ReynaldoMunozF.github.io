717f36960faec72428fbb82c37c5db107ed3c060
ReynaldoMunozF <reynaldo.munozf21@gmail.com>
2023-12-02 14:17:34 +0100

    carga de nuevos elementos

--- adviento/main.js
+++ adviento/main.js
@@ -1,18 +1,18 @@
-let rafa = document.getElementById("rafa");
-let rey = document.getElementById("rey");
+let rafa = document.getElementById("rafa2");
+let rey = document.getElementById("rey2");
 
 
 rey.addEventListener("click", function () {
-  let rspt =  prompt("adivina el nombre del rey mago").toUpperCase()
-    if (rspt == "MELCHOR"){
+  let rspt =  prompt("completa el villancico ... CAMPANA SOBRE CAMPANA Y SOBRE CAMPANA .X.X.X.X ").toUpperCase()
+    if (rspt == "UNA"){
     location.href = "sorpresaRey.html"
 }else {
     prompt("vuelve a intentarlo")
 }
 });
 rafa.addEventListener("click", function () {
-let rspt =  prompt("adivina el nombre del rey mago").toUpperCase()
-    if (rspt == "GASPAR"){
+let rspt =  prompt("completa el villancico ... LA VIRGEN SE ESTA PEINANDO, SOBRE CORTINA Y CORTINA SUS CABELLOS SON DE  .X.X.X.X ").toUpperCase()
+    if (rspt == "ORO"){
     location.href = "sorpresaRafa.html"
 }else {
     prompt("vuelve a intentarlo")
